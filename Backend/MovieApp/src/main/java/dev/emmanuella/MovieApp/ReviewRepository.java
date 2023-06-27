2275d8c62698eb68622ac0575ae5419b8207d266
ch3lla <chukwumaemmanuella03@gmail.com>
2023-06-27 21:11:25 +0100

    initial commit

---
+++ Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/ReviewRepository.java
@@ -0,0 +1,12 @@
+package dev.emmanuella.MovieApp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>
+{
+
+}
+
