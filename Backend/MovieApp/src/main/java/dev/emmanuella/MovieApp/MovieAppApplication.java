acf3493c81757ba18dd3a28baf6abc04cd5c3bce
ch3lla <chukwumaemmanuella03@gmail.com>
2023-06-30 11:56:28 +0100

    changed allowedOrigins

--- Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/MovieAppApplication.java
+++ Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/MovieAppApplication.java
@@ -19,7 +19,7 @@ public class MovieAppApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("* ")
+						.allowedOrigins("*")
 						.allowedMethods("*")
 						.allowedHeaders("*")
 						.allowCredentials(false).maxAge(3600);
