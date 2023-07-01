5ef501e7b88e8ae83a597a2db78b1fd842800a8f
ch3lla <chukwumaemmanuella03@gmail.com>
2023-07-01 13:41:06 +0100

    changed allowedOrigins

--- Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/Movie.java
+++ Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/Movie.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collection = "movies")
+@Document(collection="movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/ReviewController.java
+++ Backend/MovieApp/src/main/java/dev/emmanuella/MovieApp/ReviewController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
